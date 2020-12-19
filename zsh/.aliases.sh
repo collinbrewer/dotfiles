17fb893ce00d53d9150ace4e178d950fb68ebfb5
Collin Brewer <hello@collinbrewer.com>
2020-12-19 05:17:24 -0600

    chore: clean up aliases to lean into zsh plugins

--- zsh/.aliases.sh
+++ zsh/.aliases.sh
@@ -3,17 +3,7 @@ alias scr="screenify"
 alias settitle="printf '\033]0;%s\007'"
 
 # docker
-alias dce="docker-compose exec"
-alias dcu="docker-compose up"
-alias dcs="docker-compose stop"
-alias dcr="docker-compose restart"
-alias dcsu="docker-compose stop && docker-compose up"
 alias dka='docker kill $(docker ps -q)'
-
-# alias ctags="`brew --prefix`/bin/ctags"
-
-# alias gb="git for-each-ref --sort='-authordate' --format='%(authordate)%09%(objectname:short)%09%(refname)' refs/heads | sed -e 's-refs/heads/--'"
-alias gpu='git push -u origin `git rev-parse --abbrev-ref HEAD`'
 
 # git + fzf
 alias fgcob='git checkout $(git branch | sed "s/\*//" | fzf)'
@@ -32,18 +22,13 @@ alias fssh='ssh $(cat ~/.ssh/config | grep "Host " | grep -v "#" | sed "s/Host /
 alias open=xdg-open
 
 # wsl
-alias open=explorer.exe
-alias synctime="sudo ntpdate pool.ntp.org"
+# alias open=explorer.exe
+# alias synctime="sudo ntpdate pool.ntp.org"
+
+# osx
+# alias ctags="`brew --prefix`/bin/ctags"
 
 # appsumo
-alias asdc="docker-compose -f local.yml"
-alias asdce="docker-compose -f local.yml exec"
-
 alias as_clean_tests="sudo find . -type d -name __pycache__ -exec rm -r {} \+"
 
 alias ass='ssh asstage01 "docker ps --filter=\"name=django\" --format=\"{{.Status}}\t{{.Names}}\""'
-
-# sumo
-alias sumo-clean="rm -rf .builds && rm -rf .build && rm -rf client/build && git checkout client/build"
-alias sumo-perf="node commands/monkey-puppet.js --executablePath='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' --headless=true"
-
